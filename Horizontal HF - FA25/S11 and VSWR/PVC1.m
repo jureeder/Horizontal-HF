a067e362e46b1a94f32ee37763578d7978f6ff23
Juliette Reeder <111520182+jureeder@users.noreply.github.com>
2025-12-04 13:23:28 -0600

    Add files via upload

---
+++ Horizontal HF - FA25/S11 and VSWR/PVC1.m
@@ -0,0 +1,100 @@
+clear all, close all, clc;
+
+% Load S-parameters from s1p file
+S = sparameters('PVC24.s1p');
+freq = S.Frequencies;  % Frequency in Hz
+S11 = squeeze(S.Parameters(1,1,:));
+    
+% Convert to dB (use 20log for voltages)
+S11_dB = 20*log10(abs(S11));
+    
+% S11
+figure;
+semilogx(freq .* 10^-6, S11_dB, 'b','LineWidth',1.5);
+xlabel('Frequency (MHz)');
+ylabel('|S11| (dB)');
+title('S11');
+hold on;
+grid on;
+
+% Surrogate frequencies
+xline(14.000, 'label', '14.000');
+xline(18.068, 'label', '18.068');
+xline(21.450, 'label', '21.450');
+xline(24.890, 'label', '24.890');
+
+% Actual frequencies
+xline(13.530, 'r', 'label', '13.530');
+xline(16.060, 'r', 'label', '16.060');
+xline(17.048, 'r', 'label', '17.048');
+xline(18.036, 'r', 'label', '18.036');
+xline(19.270, 'r', 'label', '19.270');
+xline(20.510, 'r', 'label', '20.510');
+xline(21.460, 'r', 'label', '21.460');
+xline(23.180, 'r', 'label', '23.180');
+xline(24.450, 'r', 'label', '24.450');
+xline(26.875, 'r', 'label', '26.875');
+
+VSWR = (1 + abs(S11)) ./ (1 - abs(S11));
+
+% VSWR
+figure;
+semilogx(freq .* 10^-6, VSWR, 'r','LineWidth',1.5);
+xlabel('Frequency (MHz)');
+ylabel('VSWR');
+title('VSWR');
+hold on;
+grid on;
+ylim([1 10]);
+
+% Surrogate frequencies
+xline(14.025, 'label', '14.025');
+xline(14.150, 'label', '14.150');
+xline(14.225, 'label', '14.225');
+xline(14.350, 'label', '14.350');
+xline(18.068, 'label', '18.068');
+xline(18.168, 'label', '18.168');
+xline(21.025, 'label', '21.025');
+xline(21.200, 'label', '21.200');
+xline(21.275, 'label', '21.275');
+xline(21.450, 'label', '21.450');
+xline(24.890, 'label', '24.890');
+xline(24.990, 'label', '24.990');
+xline(28.000, 'label', '28.000');
+xline(29.700, 'label', '29.700');
+
+% General License Operating Regions
+%xregion(14.025, 14.150, [0.8 0.8 0.8]); % 20m
+%xregion(14.225, 14.350, [0.8 0.8 0.8]); % 20m
+%xregion(18.068, 18.168, [0.8 0.8 0.8]); % 17m
+%xregion(21.025, 21.200, [0.8 0.8 0.8]); % 15m
+%xregion(21.275, 21.450, [0.8 0.8 0.8]); % 15m
+%xregion(24.890, 24.990, [0.8 0.8 0.8]); % 12m
+%xregion(28.000, 29.700, [0.8 0.8 0.8]); % 10m
+
+Y_points = [1, 10, 10, 1];
+
+fill([14.025, 14.025, 14.150, 14.150], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+fill([14.225, 14.225, 14.350, 14.350], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+fill([18.068, 18.068, 18.168, 18.168], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+fill([21.025, 21.025, 21.200, 21.200], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+fill([21.275, 21.275, 21.450, 21.450], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+fill([24.890, 24.890, 24.990, 24.990], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+fill([28.000, 28.000, 29.700, 29.700], Y_points, [0.8 0.8 0.8], 'FaceAlpha', 0.75, 'EdgeColor', 'none'); 
+
+% Actual frequencies
+xline(13.530, 'r', 'label', '13.530');
+xline(16.060, 'r', 'label', '16.060');
+xline(17.048, 'r', 'label', '17.048');
+xline(18.036, 'r', 'label', '18.036');
+xline(19.270, 'r', 'label', '19.270');
+xline(20.510, 'r', 'label', '20.510');
+xline(21.460, 'r', 'label', '21.460');
+xline(23.180, 'r', 'label', '23.180');
+xline(24.450, 'r', 'label', '24.450');
+xline(26.875, 'r', 'label', '26.875');
+
+% Create Smith chart
+figure('Name','Smith Chart from .s1p file','NumberTitle','off');
+
+smithplot(S11);
